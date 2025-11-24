cf2bb6cda89ea26f70a2748243f01062d89c8506
Vishal Jadhav <vishaljadhav.11022002@gmail.com>
2025-11-24 18:21:18 +0530

    added new code

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -1,19 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../../assets/assets";
 
 export const StoreContext = createContext(null);
 
-const StoreContextProvider = (props ) => {
+const StoreContextProvider = (props) => {
+  const [cartItem, setCartItems] = useState({});
 
-    const ContextValue ={
-        food_list
-
+  const addToCart = (itemId) => {
+    if (!cartItem[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-    return(
-        <StoreContext.Provider value={ContextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
+  };
 
-}
+  const removeFromCart = (itemId) => {
+    setCartItems((prev)=>(  {...prev,[itemId]:prev[itemId]-1}))
+  }
+
+  const ContextValue = {
+    food_list,
+    cartItem,
+    setCartItems,
+    addToCart,
+    removeFromCart
+};
+  return (
+    <StoreContext.Provider value={ContextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 export default StoreContextProvider;
